e93f17e8345e2d338550f95541c8ad0e0ac57ae1
RaGe <foragerr@gmail.com>
2016-04-14 21:26:33 -0400

    Initial Import

---
+++ module/src/main/java/pkg/TestClass.java
@@ -0,0 +1,9 @@
+package pkg;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+
+public class TestClass{
+	int a;
+}
